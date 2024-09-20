09a08c8f79ec2b9ad668d1aa2a0befa19a45cda7
gloriamatope <52277838+gloriamatope@users.noreply.github.com>
2024-09-20 15:24:12 +0200

    added AI API migration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-function generatePoem(event) {
-    event.preventDefault();
-   
-   ;
-    new Typewriter('#poem', {
+function displayPoem(response) {
+    console.log("poem generated");
+
+     new Typewriter('#poem', {
     //strings: ['Hello', 'World', 'Gloria😚'],//erases when you use array
-    strings: 'La tombe dit a la rose',
+   
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
         delay: 1
-});
+}); 
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+    let instructionInput = document.querySelector("#user-instructions");
+    let apiKey = "c05bc3a7aca0034e2aaf87b4fb0a01ot";
+    let prompt = `User instruction are: Generate a French  poem about ${instructionInput.value}`;
+    let context = "You are a romantic Poem expert and love to write short poems.Your mission is generate a 4 line poem in basic HTML and separate each line with a < br/>.Do not include a tittle in the poem. Sign the poem with 'SheCodes AI'inside a <strong> element and do not include html tag inside the poem.Make sure to follow your user instruction.";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    console.log("Genarating poem");
+    console.log(`Prompt:${prompt}`);
+    console.log(`Prompt:${context}`);
+    
+
+        axios.get(apiUrl).then(displayPoem);
+    //Build the API URL
+    //Make a to the API
+    //Display the generated poem
+  
 }
 
 
